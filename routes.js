a88778aff7f2598c43f14482b58db0bfa6876bc5
jubamatsaberidze <j.matsaberidze.websmart@gmail.com>
2022-07-06 19:17:01 +0400

    added new feature to get all data

--- routes.js
+++ routes.js
@@ -1,18 +1,9 @@
 module.exports = (app) => {
-  const listController = require("./controllers/list.controller.js")
+  const listController = require("./controllers/list.controller.js");
   const router = require("express").Router();
-  // router.get('/?id=:id([0-9]{3})', (req, res) => {
-  //   const {id} = req.params;
-  //   res.send({id, res: "Hello guys"});
-  // })
-  router.route("/:id")
-  .delete(listController.remove);
-
+ 
   router.route("/")
-  .post(listController.add);
-
-  router.route("/get/:id")
-  .get(listController.get)
-
+  .get(listController.get);
+ 
  app.use("/api/list", router);
 }
