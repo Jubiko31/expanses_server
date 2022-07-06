218299cb52e3d5b27ef566a50cfacf7f1172f420
juba31 <53910160+Jubiko31@users.noreply.github.com>
2022-07-06 17:13:17 +0400

    updated delete data

--- controllers/list.controller.js
+++ controllers/list.controller.js
@@ -1,19 +1,5 @@
 const { list } = require("../models");
 
-exports.add = (req, res) => {
-  if (!req.body["name"].trim()) {
-    res.status(422);
-    res.send({answer: "Text is required"});
-  }
-  list.create(req.body)
-  .then(data => {
-   
-    res.send(data);
-  }).catch(err => {
-    res.status(422).send({answer: err});
-  })
-}
-
 exports.remove = (req, res) => {
   const {id} = req.params;
   if (!id.trim()) {
