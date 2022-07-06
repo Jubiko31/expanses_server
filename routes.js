764938a066dabc85f45c2e2a259a829a1871ee4b
juba31 <53910160+Jubiko31@users.noreply.github.com>
2022-07-06 18:35:28 +0400

    Update routes.js

--- routes.js
+++ routes.js
@@ -1,17 +1,13 @@
 module.exports = (app) => {
-  const todosController = require("./controllers/list.controller.js")
+  const listController = require("./controllers/list.controller.js")
   const router = require("express").Router();
-  // router.get('/?id=:id([0-9]{3})', (req, res) => {
-  //   const {id} = req.params;
-  //   res.send({id, res: "Hello guys"});
-  // })
-  router.route("/:id")
-  .get()
-  .delete()
-  .patch();
+  
+//   router.route("/:id")
+//   .get()
+//   .delete()
+//   .patch();
 
   router.route("/")
-  .get()
   .post(todosController.add);
 
   app.use("/api/list", router);
