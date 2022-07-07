b2c21abc15ed844922707e0f7633e49604fb9b7b
jubamatsaberidze <j.matsaberidze.websmart@gmail.com>
2022-07-07 09:11:26 +0400

    updated controller to catch errors if is any

--- controllers/list.controller.js
+++ controllers/list.controller.js
@@ -2,6 +2,11 @@
 const { list } = require("../models");
 
 exports.get = async (req, res) => {
+  try {
     const all = await list.findAll();
     res.json(all);
+  }
+  catch (err) {
+    console.error(err.message);
+  }
 }
