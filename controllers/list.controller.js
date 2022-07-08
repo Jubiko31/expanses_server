90d6cc473a956414074e79a96514a80474c3bb7e
jubamatsaberidze <j.matsaberidze.websmart@gmail.com>
2022-07-08 14:06:11 +0400

    added new feature to get all data

--- controllers/list.controller.js
+++ controllers/list.controller.js
@@ -1,5 +1,15 @@
 const { list } = require("../models");
 
+exports.get = async (req, res) => {
+  try {
+    const all = await list.findAll();
+    res.json(all);
+  }
+  catch (err) {
+    res.status(422).send({answer: err});
+  }
+}
+
 exports.remove = (req, res) => {
   const { id } = req.params;
   if (!id.trim()) {
--- routes.js
+++ routes.js
@@ -3,6 +3,9 @@ module.exports = (app) => {
   const listController = require("./controllers/list.controller.js")
   const router = require("express").Router();
 
+  router.route("/")
+  .get(listController.get);
+  
   router.route("/:id")
   .delete(listController.remove);
 
